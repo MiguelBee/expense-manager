154c6d3a92d2fc26ab03b44a88ca709e5e24ba38
Miguel Bustamante <mbustamante98@hotmail.com>
2024-10-11 20:21:18 -0600

    Chapt 10: State management and RenderProps.

--- src/components/ExpenseEntryItemList.js
+++ src/components/ExpenseEntryItemList.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import './ExpenseEntryItemList.css'
 
 class ExpenseEntryItemList extends React.Component {
@@ -9,6 +8,10 @@ class ExpenseEntryItemList extends React.Component {
 		this.handleMouseEnter = this.handleMouseEnter.bind();
 		this.handleMouseLeave = this.handleMouseLeave.bind();
 		this.handleMouseOver = this.handleMouseOver.bind();
+		
+		this.state = {
+			items: this.props.items
+		}
 	}
 
 	handleMouseEnter(e) {
@@ -23,30 +26,64 @@ class ExpenseEntryItemList extends React.Component {
 		console.log("The mouse is at(" + e.clientX + ", " + e.clientY + ")");
 	}
 
+	handleDelete = (id, e) => {
+		e.preventDefault();
+
+		if(this.props.onDelete != null)
+			this.props.onDelete(id, e);
+	}
+
+	getTotal() {
+		let total = 0;
+		for(var i = 0; i < this.state.items.length; i++) {
+			total += this.state.items[i].amount
+		}
+		return total;
+	}
+
+	static getDerivedStateFromProps(props, state) {
+		let newState = {
+			items: props.items
+		}
+		return newState;
+	}
+
 	render() {
-		const lists = this.props.items.map((item) =>
+		const lists = this.state.items.map((item) =>
 				<tr key={item.id} onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
-				<td>{item.name}</td>
-				<td>{item.amount}</td>
-				<td>{new Date(item.spendDate).toDateString()}</td>
-				<td>{item.category}</td>
+					<td>{item.name}</td>
+					<td>{item.amount}</td>
+					<td>{new Date(item.spendDate).toDateString()}</td>
+					<td>{item.category}</td>
+					<td><a href="#"
+						onClick={(e) => this.handleDelete(item.id, e)}>Remove</a>
+					</td>
 				</tr>
 		);
 
 		return (
-			<table onMouseOver={this.handleMouseOver}>
-					<thead>
+			<div>
+				<table onMouseOver={this.handleMouseOver}>
+						<thead>
 							<tr>
-									<th>Item</th>
-									<th>Amount</th>
-									<th>Date</th>
-									<th>Category</th>
+								<th>Item</th>
+								<th>Amount</th>
+								<th>Date</th>
+								<th>Category</th>
+								<th>Remove</th>
 							</tr>
-					</thead>
-					<tbody>
-						{lists}
-					</tbody>
-			</table>
+						</thead>
+						<tbody>
+							{lists}
+							<tr>
+								<td colSpan="1" style={{ textAlign: "right"}}> Total Amount </td>
+								<td colSpan="4" style={{ textAlign: "left" }}>
+									{this.getTotal()}
+								</td>
+							</tr>
+						</tbody>
+				</table>
+			</div>
 		);
 	}
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Pager from './components/Pager';
 import ExpenseEntryItemList from './components/ExpenseEntryItemList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -7,29 +8,42 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 const items = [
  { id: 1, name: "Pizza", amount: 80, spendDate: "2020-10-10", category: "Food"
 },
- { id: 1, name: "Grape Juice", amount: 30, spendDate: "2020-10-12", category:
+ { id: 2, name: "Grape Juice", amount: 30, spendDate: "2020-10-12", category:
 "Food" },
- { id: 1, name: "Cinema", amount: 210, spendDate: "2020-10-16", category:
+ { id: 3, name: "Cinema", amount: 210, spendDate: "2020-10-16", category:
 "Entertainment" },
- { id: 1, name: "Java Programming book", amount: 242, spendDate: "2020-10-15",
+ { id: 4, name: "Java Programming book", amount: 242, spendDate: "2020-10-15",
 category: "Academic" },
- { id: 1, name: "Mango Juice", amount: 35, spendDate: "2020-10-16", category:
+ { id: 5, name: "Mango Juice", amount: 35, spendDate: "2020-10-16", category:
 "Food" },
- { id: 1, name: "Dress", amount: 2000, spendDate: "2020-10-25", category:
+ { id: 6, name: "Dress", amount: 2000, spendDate: "2020-10-25", category:
 "Cloth" },
- { id: 1, name: "Tour", amount: 2555, spendDate: "2020-10-29", category:
+ { id: 7, name: "Tour", amount: 2555, spendDate: "2020-10-29", category:
 "Entertainment" },
- { id: 1, name: "Meals", amount: 300, spendDate: "2020-10-30", category:
+ { id: 8, name: "Meals", amount: 300, spendDate: "2020-10-30", category:
 "Food" },
- { id: 1, name: "Mobile", amount: 3500, spendDate: "2020-11-02", category:
+ { id: 9, name: "Mobile", amount: 3500, spendDate: "2020-11-02", category:
 "Gadgets" },
- { id: 1, name: "Exam Fees", amount: 1245, spendDate: "2020-11-04", category:
+ { id: 10, name: "Exam Fees", amount: 1245, spendDate: "2020-11-04", category:
 "Academic" }
 ]
 
+const pageCount = 3;
+
 root.render(
   <React.StrictMode>
-    <ExpenseEntryItemList items={items} />
+    <Pager
+      items={items}
+      pageCount={pageCount}
+      render={
+        pagerState => (
+          <div>
+            <ExpenseEntryItemList items={pagerState.items}
+              onDelete={pagerState.deleteHandler} />
+          </div>
+        )
+      }
+    />
   </React.StrictMode>
 );
 
